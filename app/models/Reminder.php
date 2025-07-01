1268c813decbcbfdff781e556c00e50b7ec88a15
Kosiso Abone <abonekosiso@gmail.com>
2025-07-01 17:40:51 +0000

    Modified getAllReminder function to implement the actual functionality of selecting all the items from the note table in the db

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -1,7 +1,14 @@
 <?php
 
 class Reminder {
-    public function getAll() {
-        echo 'reminder'; die;
+    public function getAllReminders(): array {
+        $db = db_connect();
+        $stmt = $db->prepare(
+            "SELECT * 
+               FROM notes 
+           ORDER BY created_at DESC"
+        );
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
