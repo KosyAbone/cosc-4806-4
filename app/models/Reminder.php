fe11766efe502b810dc38ef886e2eb0d5ceb7328
Kosiso Abone <abonekosiso@gmail.com>
2025-07-01 17:21:20 +0000

    started by creating a view for reminder and also a model but with basic print functionality

---
+++ app/models/Reminder.php
@@ -0,0 +1,7 @@
+<?php
+
+class Reminder {
+    public function getAll() {
+        echo 'reminder';
+    }
+}
---
+++ app/views/reminders/index.php
@@ -0,0 +1,12 @@
+<?php require_once 'app/views/templates/header.php'; ?>
+
+<div class="container">
+    <h1>Welcome, <?=$_SESSION['username'] ?>!</h1>
+    <p My Reminders</p>
+
+    <p>
+        <a href="/logout">Logout</a>
+    </p>
+</div>
+
+<?php require_once 'app/views/templates/footer.php'; ?>
